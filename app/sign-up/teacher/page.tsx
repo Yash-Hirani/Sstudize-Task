a0585cab95c167d7f091db66ef8ddc17abd0f1f3
Yash Hirani <yashhirani.0055@gmail.com>
2025-04-19 23:27:35 +0530

    feat: teacher onBoarding

--- app/sign-up/teacher/page.tsx
+++ app/sign-up/teacher/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use server";
 
-const page = () => {
+import { auth } from "@/utils/auth";
+import { redirect } from "next/navigation";
+
+const TeacherOnBoarding = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return <div>Hello Teacher!</div>;
 };
 
-export default page;
+export default TeacherOnBoarding;
