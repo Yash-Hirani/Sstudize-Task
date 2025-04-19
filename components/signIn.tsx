d762607f9440e99f499628503f5081ecfb0c24b2
Yash Hirani <yashhirani.0055@gmail.com>
2025-04-19 22:49:00 +0530

    init non-functional

---
+++ components/signIn.tsx
@@ -0,0 +1,12 @@
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+  );
+}
